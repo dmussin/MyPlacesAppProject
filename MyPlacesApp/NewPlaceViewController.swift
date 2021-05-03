21432a414edae77c6da27e3585ebc997738f58e5
Daniyar Mussin <45901913+stormy377@users.noreply.github.com>
2021-05-03 18:55:43 +0200

    Image Picker Controller (tapping on image functionality, action alert: Photo, Camera. infoPlist permission for camera use Image Selection Action ActionSheet customization(icons, text alignment)

--- MyPlacesApp/NewPlaceViewController.swift
+++ MyPlacesApp/NewPlaceViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController {
 
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,11 +27,38 @@ class NewPlaceViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
             
+            let cameraIcon = #imageLiteral(resourceName: "camera")
+            let photoIcon = #imageLiteral(resourceName: "photo")
+            
+            // after tapping on image, action sheet alert appears with the 3 options: Camera, PhotoLib and Cancel.
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.choseImagePicker(source: .camera)
+            }
+            
+            camera.setValue(cameraIcon, forKey: "image") // setting value for key(icon image)
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment") // text to the left.
+            
+            let photo = UIAlertAction(title: "Photo Library", style: .default) { _ in
+                self.choseImagePicker(source: .photoLibrary)
+                
+            }
+            
+            photo.setValue(photoIcon, forKey: "image")
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
         } else {
             view.endEditing(true)
         }
     }
-
 }
 
 // MARK: Text field delegate
@@ -42,4 +71,30 @@ extension NewPlaceViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
+}
+
+
+// MARK: Work with image
+extension NewPlaceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    // setting up source for ImagePickerController
+    func choseImagePicker(source: UIImagePickerController.SourceType){
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self // setting delegate for image selection to class
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+    
+    
+    // picking image and changing the black image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill
+        imageOfPlace.clipsToBounds = true
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
