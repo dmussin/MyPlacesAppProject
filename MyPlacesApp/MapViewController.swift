c6408172947b78a0b11bbbf6a2609718ab1a8cc9
Daniyar Mussin <45901913+stormy377@users.noreply.github.com>
2021-05-11 18:41:23 +0200

    Map View Controller
    Creating Map View Controller
    2 buttons

---
+++ MyPlacesApp/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  MyPlacesApp
+//
+//  Created by Daniyar Mussin on 11.05.2021.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+
+    @IBAction func closeVC() {
+        dismiss(animated: true, completion: nil) // closing mapVC
+    }
+}
