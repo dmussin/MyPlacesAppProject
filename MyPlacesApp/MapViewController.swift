51d99593326c500fa3c921bdb3c1e853c2ea4ccc
Daniyar Mussin <45901913+stormy377@users.noreply.github.com>
2021-05-12 17:02:39 +0200

    Annotations
    Pin, Banner, Image on the annotations in Maps.

--- MyPlacesApp/MapViewController.swift
+++ MyPlacesApp/MapViewController.swift
@@ -11,18 +11,17 @@ import MapKit
 class MapViewController: UIViewController {
     
     var place: Place!
+    let annotationIdentifier = "annotationIdentifier"// value of annotation
     
 
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         
         // calling method setupPlacemark by the transition on ViewController
-        setupPlacemark() 
-        
-
-      
+        setupPlacemark()
     }
 
     
@@ -65,5 +64,33 @@ class MapViewController: UIViewController {
         }
         
         
+    }
+}
+
+// Pin with banner in Maps.
+extension MapViewController: MKMapViewDelegate{
+    // displaying annotation
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        // my current position
+        guard !(annotation is MKUserLocation) else { return nil }
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView // coercion to PIN on map
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.canShowCallout = true   // Annotation in the form of banner
+        }
+        
+        // Placing image on banner
+        if let imageData = place.imageData {
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            imageView.layer.cornerRadius = 10
+            imageView.clipsToBounds = true
+            imageView.image = UIImage(data: imageData)
+            annotationView?.rightCalloutAccessoryView = imageView // displaying image 
+        }
+        
+        return annotationView
     }
 }
